3d58ebc65407ec021b1de7e5b37c2195ca4e5cc1
Tomas Dulka <dulajz@gmail.com>
2017-12-04 23:04:34 +0100

    oprava transformace na funkci

--- model/program/DetailAktivity.jsx
+++ model/program/DetailAktivity.jsx
@@ -1,9 +1,9 @@
-function DetailAktivity() {
-  let aktivita = this.props.aktivita;
-  let linie = this.props.linie.find(lajna => lajna.id == aktivita.linie);
+function DetailAktivity(props) {
+  let aktivita = props.aktivita;
+  let linie = props.linie.find(lajna => lajna.id == aktivita.linie);
 
   return (
-    <div className = "detail-aktivity" onClick = {() => this.props.zvolTutoAktivitu({})}>
+    <div className = "detail-aktivity" onClick = {() => props.zvolTutoAktivitu({})}>
       <h2>{aktivita.nazev}</h2>
       <span>Linie: {linie.nazev}</span>
       <br/>
