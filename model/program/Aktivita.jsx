7ba2a2a42de075dd8fe24686a4f2c22574c1b277
Tomas Dulka <dulajz@gmail.com>
2017-12-05 11:31:54 +0100

    předělána aktivita na funkci místo třídy

--- model/program/Aktivita.jsx
+++ model/program/Aktivita.jsx
@@ -1,37 +1,25 @@
-class Aktivita extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.zpracujKlik = this.zpracujKlik.bind(this);
+function Aktivita(props) {
+  let aktivita = props.aktivita;
+  let kapacita;
+  //TODO: dořešit jaké jsou přesně možnosti a aby server vracel kapacity skutečně v podobě, kterou chceme
+  if(aktivita.kapacita_u > 0) {
+    kapacita = <span>({aktivita.prihlaseno_f+aktivita.prihlaseno_m}/{aktivita.kapacita_u})</span>;
+  }
+  else if(aktivita.kapacita_m > 0 && aktivita.kapacita_f > 0) {
+    kapacita = <span>({aktivita.prihlaseno_f}/{aktivita.kapacita_f})({aktivita.prihlaseno_m}/{aktivita.kapacita_m})</span>;
+  }
+  else if(aktivita.kapacita_m > 0 && aktivita.kapacita_f == 0) {
+    kapacita = <span>({aktivita.prihlaseno_m}/{aktivita.kapacita_m})</span>;
+  }
+  else if(aktivita.kapacita_m == 0 && aktivita.kapacita_f > 0) {
+    kapacita = <span>({aktivita.prihlaseno_f}/{aktivita.kapacita_f})</span>;
   }
 
-  zpracujKlik() {
-    this.props.zvolTutoAktivitu(this.props.aktivita);
-  }
-
-  render() {
-    let aktivita = this.props.aktivita;
-    let kapacita;
-    //TODO: dořešit jaké jsou přesně možnosti a aby server vracel kapacity skutečně v podobě, kterou chceme
-    if(aktivita.kapacita_u > 0) {
-      kapacita = <span>({aktivita.prihlaseno_f+aktivita.prihlaseno_m}/{aktivita.kapacita_u})</span>;
-    }
-    else if(aktivita.kapacita_m > 0 && aktivita.kapacita_f > 0) {
-      kapacita = <span>({aktivita.prihlaseno_f}/{aktivita.kapacita_f})({aktivita.prihlaseno_m}/{aktivita.kapacita_m})</span>;
-    }
-    else if(aktivita.kapacita_m > 0 && aktivita.kapacita_f == 0) {
-      kapacita = <span>({aktivita.prihlaseno_m}/{aktivita.kapacita_m})</span>;
-    }
-    else if(aktivita.kapacita_m == 0 && aktivita.kapacita_f > 0) {
-      kapacita = <span>({aktivita.prihlaseno_f}/{aktivita.kapacita_f})</span>;
-    }
-
-    return (
-      <td colSpan={aktivita.delka} className = "tabulka-aktivita" onClick = {this.zpracujKlik}>
-        <span>{aktivita.nazev}</span>
-        <br/>
-        {kapacita}
-      </td>
-    );
-  }
+  return (
+    <td colSpan={aktivita.delka} className = "tabulka-aktivita" onClick = {() => props.zvolTutoAktivitu(aktivita)}>
+      <span>{aktivita.nazev}</span>
+      <br/>
+      {kapacita}
+    </td>
+  );
 }
