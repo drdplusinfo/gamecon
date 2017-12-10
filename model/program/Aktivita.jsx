f1a0dd260c94bb1ee6c442ce9da0897db593b3be
Tomas Dulka <dulajz@gmail.com>
2017-12-10 18:01:41 +0100

    sprovoznen vypocet kapacity pohlavi

--- model/program/Aktivita.jsx
+++ model/program/Aktivita.jsx
@@ -1,25 +1,49 @@
 function Aktivita(props) {
   let aktivita = props.aktivita;
-  let kapacita;
-  //TODO: dořešit jaké jsou přesně možnosti a aby server vracel kapacity skutečně v podobě, kterou chceme
-  if(aktivita.kapacita_u > 0) {
-    kapacita = <span>({aktivita.prihlaseno_f+aktivita.prihlaseno_m}/{aktivita.kapacita_u})</span>;
-  }
-  else if(aktivita.kapacita_m > 0 && aktivita.kapacita_f > 0) {
-    kapacita = <span>({aktivita.prihlaseno_f}/{aktivita.kapacita_f})({aktivita.prihlaseno_m}/{aktivita.kapacita_m})</span>;
-  }
-  else if(aktivita.kapacita_m > 0 && aktivita.kapacita_f == 0) {
-    kapacita = <span>({aktivita.prihlaseno_m}/{aktivita.kapacita_m})</span>;
-  }
-  else if(aktivita.kapacita_m == 0 && aktivita.kapacita_f > 0) {
-    kapacita = <span>({aktivita.prihlaseno_f}/{aktivita.kapacita_f})</span>;
+  //názvy tříd u kapacity
+  const ZENY = "zeny";
+  const MUZI = "muzi";
+  const UNIVERZALNI = "univerzalni";
+  //obalující div
+  const KAPACITA = "kapacita";
+
+  function zjistiKapacitu(aktivita) {
+    let {prihlaseno_f, prihlaseno_m, kapacita_f, kapacita_m, kapacita_u} = aktivita;
+    if(kapacita_f + kapacita_m + kapacita_u <= 0) {
+      //aktivita bez omezení
+      return null;
+    }
+    else if(prihlaseno_f + prihlaseno_m >= kapacita_f + kapacita_m + kapacita_u) {
+      //beznadějně plno
+      let zeny = prihlaseno_f > 0 ? <span className = {ZENY}>({prihlaseno_f}/{prihlaseno_f})</span> : '';
+      let muzi = prihlaseno_m > 0 ? <span className = {MUZI}>({prihlaseno_m}/{prihlaseno_m})</span> : '';
+      return <div className = {KAPACITA}>{zeny}{muzi}</div>;
+    }
+    else if(prihlaseno_m >= kapacita_m + kapacita_u) {
+      //muži zabrali všechny svá i univerzální místa
+      let zeny = kapacita_f > 0 ? <span className = {ZENY}>({prihlaseno_f}/{kapacita_f})</span> : '';
+      let muzi = <span className = {MUZI}>({prihlaseno_m}/{prihlaseno_m})</span>;
+      return <div className = {KAPACITA}>{zeny}{muzi}</div>;
+    }
+    else if(prihlaseno_f >= kapacita_f + kapacita_u) {
+      //ženy zabrali všechny svá i univerzální místa
+      let zeny = <span className = {ZENY}>({prihlaseno_f}/{prihlaseno_f})</span>;
+      let muzi = kapacita_m > 0 ? <span className = {MUZI}>({prihlaseno_m}/{kapacita_m})</span> : '';
+      return <div className = {KAPACITA}>{zeny}{muzi}</div>;
+    }
+    else {
+      //jinak volno, žádné pohlaví nevyžralo limit míst
+      let univerzalni = <span className = {UNIVERZALNI}>({prihlaseno_f+prihlaseno_m}/{kapacita_f+kapacita_m+kapacita_u})</span>
+      return <div className = {KAPACITA}>{univerzalni}</div>
+    }
+
   }
 
   return (
     <td colSpan={aktivita.delka} className = "tabulka-aktivita" onClick = {() => props.zvolTutoAktivitu(aktivita)}>
       <span>{aktivita.nazev}</span>
       <br/>
-      {kapacita}
+      {zjistiKapacitu(aktivita)}
     </td>
   );
 }
