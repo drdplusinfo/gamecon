f253d4ae4b1cd4e337e4f7ba9a2553f90673fe3f
Tomas Dulka <dulajz@gmail.com>
2018-01-30 22:15:39 +0100

    refaktoring tlacitka prihlasit

--- model/program/TlacitkoPrihlasit.jsx
+++ model/program/TlacitkoPrihlasit.jsx
@@ -10,80 +10,144 @@ class TlacitkoPrihlasit extends React.Component {
     this.zavriModal = this.zavriModal.bind(this);
   }
 
-  aktivitaPlna() {
-    let akt = this.props.aktivita;
-    if (akt.kapacitaZeny + akt.kapacitaMuzi + akt.kapacitaUniverzalni === 0) {
-      //když je kapacita nula, aktivita je bez omezení
+  aktivitaJePlna(aktivita) {
+    if (this.aktivitaJeBezOmezeni(aktivita)) {
       return false;
     }
-
-    if (akt.prihlasenoMuzu + akt.prihlasenoZen >= akt.kapacitaZeny + akt.kapacitaMuzi + akt.kapacitaUniverzalni) {
-      //beznadějně plno
+    if (this.aktivitaJeBeznadejnePlna(aktivita)) {
       return true;
     }
 
-    //testujeme kapacitu pro uživatelovo pohlaví
-    let pohlavi = this.props.uzivatelPohlavi;
-    if (pohlavi === "m" && akt.prihlasenoMuzu >= akt.kapacitaMuzi + akt.kapacitaUniverzalni) {
-      return true;
-    }
-    if (pohlavi === "f" && akt.prihlasenoZen >= akt.kapacitaZeny + akt.kapacitaUniverzalni) {
-      return true;
-    }
+    return this.aktivitaJePlnaProPohlaviUzivatele(aktivita, this.props.uzivatelPohlavi);
+  }
 
+  aktivitaJeBezOmezeni(aktivita) {
+    return aktivita.kapacitaZeny + aktivita.kapacitaMuzi + aktivita.kapacitaUniverzalni === 0;
+  }
+
+  aktivitaJeBeznadejnePlna(aktivita) {
+    return aktivita.prihlasenoMuzu + aktivita.prihlasenoZen >= aktivita.kapacitaZeny + aktivita.kapacitaMuzi + aktivita.kapacitaUniverzalni;
+  }
+
+  aktivitaJePlnaProPohlaviUzivatele(aktivita, pohlavi) {
+    if (pohlavi === "m" && aktivita.prihlasenoMuzu >= aktivita.kapacitaMuzi + aktivita.kapacitaUniverzalni) {
+      return true;
+    }
+    if (pohlavi === "f" && aktivita.prihlasenoZen >= aktivita.kapacitaZeny + aktivita.kapacitaUniverzalni) {
+      return true;
+    }
     return false;
   }
 
-  muzeKlikatTlacitko() {
-    let akt = this.props.aktivita;
-    //metoda zjišťuje, jestli se tlačítko přihlásit vůbec má zobrazit
-    if (akt.prihlasen) {
+  aktivitaJePrazdnaATymova(aktivita) {
+    return aktivita.tymova  // TODO: v ostré verzi přidat && aktivita.prihlasenoZen + aktivita.prihlasenoMuzu === 0;
+  }
+
+  aktivitaJeZkolapsovanyDracak(aktivita) {
+    return aktivita.nazev.includes('Čtvrtfinále');
+  }
+
+  maBytTlacitkoZobrazene() {
+    const aktivita = this.props.aktivita;
+
+    if (this.muzeSeOdhlasit(aktivita)) {
       return true;
     }
 
-    if (akt.organizuje || !akt.otevrenoPrihlasovani || akt.probehnuta) {
+    if (this.muzeSeOdhlasitJakoNahradnik(aktivita)) {
+      return true;
+    }
+
+    if (this.proUzivateleJeAktivitaNepristupna(aktivita)) {
+      //Vykomentováno kvůli testování. TODO: opravit v plné verzi
       //return false;
     }
 
-    //TODO mělo by být akt.nahradnictviMozne když to tam bude
+    if (this.aktivitaJePlna(aktivita)) {
+      return this.muzeSePrihlasitJakoNahradnik();
+    } 
+
+    return true;     
+  }
+
+  muzeSeOdhlasit(aktivita) {
+    return aktivita.prihlasen;
+  }
+
+  muzeSeOdhlasitJakoNahradnik(aktivita) {
+    //TODO zkontrolovat
+    return aktivita.prihlasenJakoNahradnik;
+  }
+
+  muzeSePrihlasitJakoNahradnik(aktivita) {
+    //TODO mělo by být aktivita.nahradnictviMozne když to bude v datech
     let nahradnictviMozne = false;
-    if (this.aktivitaPlna() && !nahradnictviMozne) {
-      return false;
-    }
-
-    return true;
+    return nahradnictviMozne;
   }
 
-  odhlas() {
-    this.props.api.odhlas(this.props.aktivita.id);
+  proUzivateleJeAktivitaNepristupna(aktivita) {
+    return aktivita.organizuje || aktivita.probehnuta || !aktivita.otevrenoPrihlasovani;
+  } 
+
+  odhlasZAktivity(aktivita) {
+    this.props.api.odhlas(aktivita.id);
   }
 
-  prihlas() {
-    let akt = this.props.aktivita;
-    //TODO: doplň check na prvního přihlášeného
-    if (akt.tymova) {
-      if (akt.nazev.includes('Čtvrtfinále')) {
-        this.setState({drdModal: true});
-      }
-      else {
-      //když je týmová a první přihlášený, ukaž modal
-      this.setState({tymovyModal: true});
+  prihlasNaAktivitu(aktivita) {
+    if (this.aktivitaJeZkolapsovanyDracak(aktivita)) {
+      this.setState({drdModal: true});
       return;
-      //reálně přihlaš
-      this.props.api.prihlas(akt.id);
-      }
     }
-  return;
+
+    if (this.aktivitaJePrazdnaATymova(aktivita)) {
+     //když je týmová a první přihlášený, ukaž modal
+     //TODO: doplň check na prvního přihlášeného
+     this.setState({tymovyModal: true});
+     return;
+    }
+
+    //reálně přihlaš
+    this.props.api.prihlas(aktivita.id);
   }
 
   prihlasOdhlas(event) {
     event.stopPropagation();
 
     if (this.props.aktivita.prihlasen) {
-      this.odhlas();
+      this.odhlasZAktivity(this.props.aktivita);
     } else {
-      this.prihlas();
+      this.prihlasNaAktivitu(this.props.aktivita);
     }
+  }
+
+  urciTextTlacitka() {
+    const aktivita = this.props.aktivita;
+
+    if (this.muzeSeOdhlasit(aktivita)) {
+      return 'Odhlásit';
+    }
+
+    if (this.muzeSeOdhlasitJakoNahradnik(aktivita)) {
+      return 'Odhlásit jako náhradník';
+    }
+
+    if (this.aktivitaJePlna(aktivita) && this.muzeSePrihlasitJakoNahradnik(aktivita)) {
+      return 'Přihlásit jako náhradník';
+    }
+
+    return 'Přihlásit';
+  }
+
+  vytvorTlacitko() {
+    if (this.maBytTlacitkoZobrazene()) {
+      return (
+        <button onClick = {this.prihlasOdhlas} className = {this.props.trida}>
+          {this.urciTextTlacitka()}
+        </button>
+      );
+    }
+    
+    return null;   
   }
 
   zavriModal() {
@@ -91,29 +155,21 @@ class TlacitkoPrihlasit extends React.Component {
   }
 
   render() {
-    let tlacitko = this.muzeKlikatTlacitko() ?
-     <button onClick = {this.prihlasOdhlas} className = {this.props.trida}>
-       {this.props.aktivita.prihlasen ? 'Odhlásit' : 'Přihlásit'}
-     </button>
-     : null;
-
     return (
-       <div>
-         {tlacitko}
-         {this.props.aktivita.tymova &&
-           <TymovyModal
-             aktivita = {this.props.aktivita}
-             api = {this.props.api}
-             zavriModal = {this.zavriModal}
-             zobrazen = {this.state.tymovyModal}
-           />
-        }
+      <div>
+        {this.vytvorTlacitko()}
+        <TymovyModal
+          aktivita = {this.props.aktivita}
+          api = {this.props.api}
+          zavriModal = {this.zavriModal}
+          zobrazen = {this.state.tymovyModal}
+        />
         <DrdModal
           aktivita = {this.props.aktivita}
           zavriModal = {this.zavriModal}
           zobrazen = {this.state.drdModal}
         />
-       </div>
+      </div>
     );
   }
 
