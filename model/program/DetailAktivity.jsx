2337043694bfd36a1bf841930130c902d9fe22c0
Tomas Dulka <dulajz@gmail.com>
2018-02-05 18:16:16 +0100

    pridani volani nactiDetail do detailu aktivity, drobny refaktoring

--- model/program/DetailAktivity.jsx
+++ model/program/DetailAktivity.jsx
@@ -1,58 +1,50 @@
 class DetailAktivity extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      dlouhyPopis: ' '
-    };
-  }
-
   componentDidMount() {
-    this.nactiPopis();
+    this.nactiDetail();
   }
 
   componentWillReceiveProps(nextProps) {
     if(nextProps.zvolenaAktivita !== this.props.zvolenaAktivita) {
-      this.nactiPopis();
+      this.nactiDetail();
     }
   }
 
-  nactiPopis() {
-    this.setState({dlouhyPopis: ''})
-    this.props.api.detail(this.props.zvolenaAktivita, (data) => {
-      this.setState({dlouhyPopis: data.popis});
-    });
+  nactiDetail() {
+    this.props.api.nactiDetail(this.props.zvolenaAktivita);
   }
 
   //udělěj element pro štítky každé kategorie(kde nějaké štítky jsou)
   //a element pro ostatní štítky
   vykresliStitky(aktivita) {
-    let stitky = this.ziskejStitky(aktivita);
-    let kategorie = stitky.kategorie.filter(kat => kat.stitky.length > 0);
-    kategorie = kategorie.sort((katA, katB) => katA.poradi - katB.poradi);
-    kategorie = kategorie.map((kat, index) => {
+    let kategorizovaneStitky = this.priradStitkyAktivityDoKategorii(aktivita);
+    
+    let naplneneKategorie = kategorizovaneStitky.kategorie.filter(kat => kat.stitky.length > 0);
+    let serazeneKategorie = naplneneKategorie.sort((katA, katB) => katA.poradi - katB.poradi);
+
+    let kategorieJakoElementy = serazeneKategorie.map((kat, index) => {
       return (
         <p key = {index}>{kat.nazev + ": " + kat.stitky.join(", ")}</p>
       );
     });
+
     return (
       <div>
-        {kategorie}
-        <p>{stitky.ostatni.join(", ")}</p>
+        {kategorieJakoElementy}
+        <p>{kategorizovaneStitky.ostatni.join(", ")}</p>
       </div>
     );
   }
 
-  ziskejStitky(aktivita) {
+  priradStitkyAktivityDoKategorii(aktivita) {
     let testStitky = [
       "Systém: Kdo z koho", "Systém: Oko za oko", "Herní styl: komedie", "Herní styl: na Buchtíka", "Herní styl: absurdní drama",
-      "Žánr: epika", "Žánr: whatever", "Prostředí: Maníkův barák", "Prostředí: My little pony", "Prostředí: julo:pomstitel",
+      "Žánr: epika", "Žánr: whatever", "Prostředí: Maníkův barák", "Prostředí: My little pony", "Prostředí: julo: pomstitel",
       "další štítek", "pro pokročilé", "pro zkušené", "nechceme nikoho"
     ];
 
     //TODO: v ostré verzi změnit na stitky = aktivita.stitky
     let stitky = testStitky;
-    //kategorie by měly jít lehce přidávat a měnit pořadí
+
     let kategorie = [
       {
         nazev: "Systém",
@@ -86,9 +78,11 @@ class DetailAktivity extends React.Component {
       kategorie.forEach(kat => {
         let regex = new RegExp(kat.nazev + ': (.*)');
         let match = stitek.match(regex);
+        
         if(match) {
-          kat.stitky.push(match[1]);
-          patriDoKategorie = true;
+          let castZaDvouteckou = match[1];
+          kat.stitky.push(castZaDvouteckou);
+          patriDoKategorie = true; 
         }
       });
 
@@ -103,33 +97,50 @@ class DetailAktivity extends React.Component {
     };
   }
 
+  vytvorZaviraciTlacitko() {
+    return <button onClick = {() => this.props.zvolTutoAktivitu({})}>x</button>
+  }
+
+  zobrazDlouhyPopisNeboLoader(aktivita) {
+    if (aktivita.popisDlouhy) {
+      return <p dangerouslySetInnerHTML = {{__html: aktivita.popisDlouhy}}></p> 
+    } else {
+      return <Loader />;
+    }
+  }
+
   render() {
     let aktivita = this.props.data.aktivity.find(akt => akt.id === this.props.zvolenaAktivita);
     let linie = this.props.data.linie.find(lajna => lajna.id == aktivita.linie);
 
     return (
       <div className = "detail-aktivity">
-        <button onClick = {() => this.props.zvolTutoAktivitu({})}>x</button>
+        {this.vytvorZaviraciTlacitko()}
+        
         <h2>{aktivita.nazev}</h2>
         <p>Linie: {linie.nazev}</p>
+
         <p>---</p>
+        
         {this.vykresliStitky(aktivita)}
+        
         <p>---</p>
+        
         <p>Vypravěč: {aktivita.organizatori.join(', ')}</p>
         <p>{aktivita.popisKratky}</p>
+
         <TlacitkoPrihlasit
           aktivita = {aktivita}
           api = {this.props.api}
           trida = 'tlacitko-prihlasit--detail'
           uzivatelPohlavi = {this.props.data.uzivatelPohlavi}
         />
+        
         <p>---</p>
+        
         <div>
-          {this.state.dlouhyPopis ?
-            <p>{this.state.dlouhyPopis}</p>
-            : <Loader />
-          }
-        </div>
+          {this.zobrazDlouhyPopisNeboLoader(aktivita)}
+        </div>        
       </div>
     );
   }
