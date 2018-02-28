9686306c04d3d385c510c32623c0cabe7e0e4e72
Manik <dmanik@seznam.cz>
2018-02-28 16:42:38 +0100

    nové coverphoto a infopruh v titulce

--- web/soubory/aplikace.js
+++ web/soubory/aplikace.js
@@ -1,46 +1,67 @@
 /* -------------- ODKAZY NA NOVOU ZÁLOŽKU ------------------- */
-$(function(){
-  $('a[href^="http"]').attr('target', '_blank');
-});
+$(function () {
+  $('a[href^="http"]').attr('target', '_blank')
+})
+
+/* -------------- ZMĚNA POZADÍ MENU NA MOBILU ---------------- */
+function zmenPozadiNavbar (){
+  if (document.getElementById('hlavniMenu').style.background === 'black') {
+    document.getElementById('hlavniMenu').style.background = 'linear-gradient(black, rgba(255,255,255,0))'
+    document.getElementById('hlavniMenu').style.marginBottom = '-51px'
+  } else {
+    document.getElementById('hlavniMenu').style.background = 'black'
+    document.getElementById('hlavniMenu').style.marginBottom = '0px'
+  }
+  return false
+};
+
+/* Změna nápisu na homepage na mobilu */
+function zmenZakladniInfo () {
+  console.log('funkce')
+  if (window.matchMedia('(max-width: 576px)').matches) {
+    console.log('funkce1')
+    document.getElementsByClassName('zakladniInfo_box-cislo-gamecon')[0].innerHTML = 'Gamecon'
+  } else {
+    console.log('funkce2')
+    document.getElementsByClassName('zakladniInfo_box-cislo-gamecon')[0].innerHTML = 'GC'
+  }
+}
 
 /* -------------- ODPOČET NA TITULCE ------------------- */
-function odpocet(cas){
+function odpocet(cas) {
   if (cas) {
-    var countDownDate = new Date(cas).getTime();
-  }
-  else {
-    document.getElementById("infopruh_odpocet").style.display = "none";
+    var countDownDate = new Date(cas).getTime()
+  } else {
+    document.getElementById('infopruh_odpocet').style.display = 'none'
   }
 
   function countdown() {
-    var now = new Date().getTime(); // Get todays date and time
-    var distance = countDownDate - now; // Find the distance between now an the count down date
+    var now = new Date().getTime() // Get todays date and time
+    var distance = countDownDate - now // Find the distance between now an the count down date
 
     // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24))
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000)
 
     // Display the result in the elements
     if (distance > 0) {
-      document.getElementById("odpocetDny").innerHTML = days;
-      document.getElementById("odpocetHodiny").innerHTML = hours;
-      document.getElementById("odpocetMinuty").innerHTML = minutes;
-      document.getElementById("odpocetSekundy").innerHTML = seconds;
-    }
-    else {
-      document.getElementById("odpocetDny").innerHTML = "0";
-      document.getElementById("odpocetHodiny").innerHTML = "0";
-      document.getElementById("odpocetMinuty").innerHTML = "0";
-      document.getElementById("odpocetSekundy").innerHTML = "0";
+      document.getElementById('odpocetDny').innerHTML = days
+      document.getElementById('odpocetHodiny').innerHTML = hours
+      document.getElementById('odpocetMinuty').innerHTML = minutes
+      document.getElementById('odpocetSekundy').innerHTML = seconds
+    } else {
+      document.getElementById('odpocetDny').innerHTML = '0'
+      document.getElementById('odpocetHodiny').innerHTML = '0'
+      document.getElementById('odpocetMinuty').innerHTML = '0'
+      document.getElementById('odpocetSekundy').innerHTML = '0'
     }
   }
 
-  countdown();
-  var intervalID = setInterval(countdown, 1000); // Aktualizuj odpočet každých 5 sekund
+  countdown()
+  setInterval(countdown, 1000) // Aktualizuj odpočet každých 5 sekund
 };
-
 
 /* -------------- MASONRY ------------------- */
 function displayMasonry() { //spočítej masonry a zobraz
