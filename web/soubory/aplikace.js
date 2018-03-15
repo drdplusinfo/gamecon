2ad70a77427d20f0c3e5efc8323d99893fdad721
Manik <dmanik@seznam.cz>
2018-03-15 10:14:18 +0100

    volání funkce inlineSVG přímo v indexu

--- web/soubory/aplikace.js
+++ web/soubory/aplikace.js
@@ -73,7 +73,6 @@ function odpocet(cas) {
 
 /* -------------- MASONRY ------------------- */
 function displayMasonry (gridName, columnWidth = 360) { // spočítej masonry a zobraz
-  console.log('Funkce displayMasonry se zavolala')
   $grid = $(gridName).masonry({
     columnWidth: columnWidth,
     itemSelector: '.grid-item',
@@ -83,60 +82,59 @@ function displayMasonry (gridName, columnWidth = 360) { // spočítej masonry a 
   return $grid;
 }
 
-function displayToggleMasonry(aktivita, gridName) { //zobraz popis u aktivity a přepočítej celé masonry
-  var $grid = displayMasonry(gridName);
-  $('#'+aktivita+'_popis').slideToggle(200);
-  setTimeout(function(){
-    $grid.masonry('layout');
-  }, 50);
-  setTimeout(function(){
-    $grid.masonry('layout');
-  }, 100);
-  setTimeout(function(){
-    $grid.masonry('layout');
-  }, 200);
-  $('#'+aktivita+'_sipka_dolu').toggle();
-  $('#'+aktivita+'_sipka_nahoru').toggle();
-  return false;
+function displayToggleMasonry (aktivita, gridName) { // zobraz popis u aktivity a přepočítej celé masonry
+  var $grid = displayMasonry(gridName)
+  $('#' + aktivita + '_popis').slideToggle(200)
+  setTimeout(function () {
+    $grid.masonry('layout')
+  }, 50)
+  setTimeout(function () {
+    $grid.masonry('layout')
+  }, 100)
+  setTimeout(function () {
+    $grid.masonry('layout')
+  }, 200)
+  $('#' + aktivita + '_sipka_dolu').toggle()
+  $('#' + aktivita + '_sipka_nahoru').toggle()
+  return false
 }
 
 /* -------------- SVG ZA INLINE SVG ------------------- */
 /*
  * Převede všechny SVG obrázky na inline SVG
  */
- window.onload = function () {
-   jQuery('img').filter(function() {
-     return this.src.match(/.*\.svg$/);
-   }).each(function(){
-     var $img = jQuery(this);
-     var imgID = $img.attr('id');
-     var imgClass = $img.attr('class');
-     var imgURL = $img.attr('src');
+function svgToInlineSvg () {
+   jQuery('img').filter(function () {
+     return this.src.match(/.*\.svg$/)
+   }).each(function () {
+     var $img = jQuery(this)
+     var imgID = $img.attr('id')
+     var imgClass = $img.attr('class')
+     var imgURL = $img.attr('src')
 
-     jQuery.get(imgURL, function(data) {
+     jQuery.get(imgURL, function (data) {
        // Get the SVG tag, ignore the rest
-       var $svg = jQuery(data).find('svg');
+       var $svg = jQuery(data).find('svg')
 
        // Add replaced image's ID to the new SVG
-       if(typeof imgID !== 'undefined') {
-         $svg = $svg.attr('id', imgID);
+       if (typeof imgID !== 'undefined') {
+         $svg = $svg.attr('id', imgID)
        }
        // Add replaced image's classes to the new SVG
-       if(typeof imgClass !== 'undefined') {
-         $svg = $svg.attr('class', imgClass+' replaced-svg');
+       if (typeof imgClass !== 'undefined') {
+         $svg = $svg.attr('class', imgClass+' replaced-svg')
        }
 
        // Remove any invalid XML tags as per http://validator.w3.org
-       $svg = $svg.removeAttr('xmlns:a');
+       $svg = $svg.removeAttr('xmlns:a')
 
        // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
-       if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+       if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
          $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
        }
 
        // Replace image with new SVG
-       $img.replaceWith($svg);
-
-     }, 'xml');
-   });
+       $img.replaceWith($svg)
+     }, 'xml')
+   })
  }
