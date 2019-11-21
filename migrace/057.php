b1ba34235e55b309cc16f34e855ffffca665b5c3
Jaroslav Týc <mail@jaroslavtyc.com>
2019-11-21 09:54:05 +0100

    Quick reports converted to universals

--- migrace/057.php
+++ migrace/057.php
@@ -1,29 +1,24 @@
 <?php
-try {
-  /** @var \Godric\DbMigrations\Migration $this */
-  $this->q(<<<SQL
-ALTER TABLE stranky
-ADD COLUMN url_prefix CHAR(10) NOT NULL DEFAULT '' AFTER url_stranky,
-ADD INDEX url_prefix(url_prefix);
-SQL
-  );
-  $this->q(<<<SQL
-CREATE TEMPORARY TABLE IF NOT EXISTS url_prefixes_temp (
-    new_url_prefix CHAR(10) PRIMARY KEY
-);
+/** @var \Godric\DbMigrations\Migration $this */
 
-INSERT IGNORE INTO url_prefixes_temp(new_url_prefix) VALUES ('drd'), ('legendy'), ('rpg'), ('larpy');
-
-UPDATE stranky
-    JOIN url_prefixes_temp
-SET stranky.url_prefix = new_url_prefix
-WHERE stranky.url_stranky LIKE CONCAT(url_prefixes_temp.new_url_prefix, '%')
+/*$this->q(<<<SQL
+DELETE FROM reporty
+WHERE id IN (
+            6,  -- Finance: Lidé v databázi + zůstatky
+            7,  -- DrD: Historie účasti
+            11, -- Zázemí & Program: Zařízení místností
+            12, -- Finance: Aktivity negenerující slevu
+            14, -- Finance: Příjmy a výdaje infopulťáka
+            34, -- DrD: Seznam přihlášených pro aktuální rok
+            35, -- Program: Hoňko report 2019
+            36, -- Program: Emaily na vypravěče dle linií
+            37, -- Program: Emaily na účastníky dle linií
+            38, -- Program: Aktivity pro dotazník dle linií
+            40, -- Potvrzení pro návštěvníky mladší patnácti let
+            41, -- Zázemí & Program: Časy a umístění aktivit
+            42, -- Zázemí & Program: Přehled místností
+            43, -- Seznam účastníků a triček 2019
+            44  -- Seznam účastníků a triček 2019 (Grouped)
+)
 SQL
-  );
-} catch (\Exception $exception) {
-  throw new RuntimeException(
-    sprintf("Migration %s failed: '%s'. Check it: \n%s", basename(__FILE__, '.php'), $exception->getMessage(), $query),
-    $exception->getCode(),
-    $exception
-  );
-}
+);*/
